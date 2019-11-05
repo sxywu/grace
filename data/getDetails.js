7f258035f37cf7d55fc87dfa31c66d132e22a53e
Shirley Wu <shirleyxueyangwu@gmail.com>
2019-11-05 15:52:15 -0500

    easily change file names

--- data/getDetails.js
+++ data/getDetails.js
@@ -2,8 +2,12 @@ const fs = require('fs')
 const _ = require('lodash')
 const wiki = require('wikijs').default
 
-const allNames = JSON.parse(fs.readFileSync('./data/names.json', {encoding: 'utf8'}))
-const details = JSON.parse(fs.readFileSync('./data/details.json', {encoding: 'utf8'})) || []
+const namesFile = '29names'
+const detailsFile = '29details'
+
+const allNames = JSON.parse(fs.readFileSync(`./data/${namesFile}.json`, {encoding: 'utf8'}))
+const details = fs.existsSync(`./data/${detailsFile}.json`) ?
+  JSON.parse(fs.readFileSync(`./data/${detailsFile}.json`, {encoding: 'utf8'})) : []
 const existingNames = _.map(details, 'name')
 console.log(allNames.length, existingNames.length)
 
@@ -32,7 +36,7 @@ function getDetails(names) {
         })
 
         console.log(`writing ${name}`)
-        fs.writeFileSync('./data/details.json', JSON.stringify(details), {encoding: 'utf8'})
+        fs.writeFileSync(`./data/${detailsFile}.json`, JSON.stringify(details), {encoding: 'utf8'})
         if (index < names.length) {
           index += 1
           getDetails(names)
