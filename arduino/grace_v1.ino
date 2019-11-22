0614fd7d490e298b8a8876e740b86a3f6eb82599
Shirley Wu <shirleyxueyangwu@gmail.com>
2019-11-22 00:18:19 -0500

    Make it work for neopixels

--- arduino/grace_v1.ino
+++ arduino/grace_v1.ino
@@ -1,29 +1,36 @@
-int count = 1;
-int outputPins[1] = {3};
-int inputPins[1] = {2};
-int brightnesses[1]; // keep track of pin brightness
-int inputVals[1];
-const int initialBrightness = 25;
+#include <Adafruit_NeoPixel.h>
+
+int count = 5;
+int outputPins[5] = {3, 5, 6, 9, 10}; // array of pin numbers for LED/neopixels
+Adafruit_NeoPixel neopixels[5];
+int brightnesses[5]; // keep track of pin brightness
+int inputPins[5] = {2}; // array of pin numbers for tilt switches
+int inputVals[5]; // keep track of which orb is being interacted with
+const int initialBrightness = 0;
 const int maxBrightness = 255;
 const int addBrightness = 10;
 
 void setup() {
   Serial.begin(9600);
-  
-  for (byte i = 0; i <= count; i += 1) {
+  Serial.println("setup");
+  while (!Serial);
+    
+  for (byte i = 0; i < count; i += 1) {
     // 1. set default brightness
     brightnesses[i] = initialBrightness;
     // 2. register output pins
-    pinMode(outputPins[i], OUTPUT);
-    // 3. register input pins
-    pinMode(inputPins[i], INPUT);
-    // 4. set all input values to default 0
-    inputVals[i] = 0;
+    neopixels[i] = Adafruit_NeoPixel(1, outputPins[i]);
+    neopixels[i].begin();
+    Serial.println(neopixels[i].getPin());
+//    // 3. register input pins
+//    pinMode(inputPins[i], INPUT);
+//    // 4. set all input values to default 0
+//    inputVals[i] = 0;
   }
 }
 
 void loop() {
-  readSwitches();
+//  readSwitches();
   writeLights();
   delay(100);
 }
@@ -32,27 +39,28 @@ void writeLights() {
   for (byte i = 0; i < count; i += 1) {
     if (inputVals[i]) {
        // if being pressed
-      analogWrite(outputPins[i], maxBrightness);
+      neopixels[i].setPixelColor(0, brightnesses[i], brightnesses[i], brightnesses[i]);
     } else {
-      analogWrite(outputPins[i], brightnesses[i]);
+      neopixels[i].setPixelColor(0, brightnesses[i], brightnesses[i], brightnesses[i]);
     }
+    neopixels[i].show();
   }
 }
-
-void readSwitches() {
-  for (byte i = 0; i < count; i += 1) {
-    // READ switch states
-    int value = digitalRead(inputPins[i]);
-
-    // if on, make sure it was off before
-    if (value && !inputVals[i]) {
-      // then add brightness
-      brightnesses[i] += addBrightness;
-      // make sure it fits in the range of LED output
-      brightnesses[i] = constrain(brightnesses[i], initialBrightness, maxBrightness - initialBrightness);
-      Serial.println(brightnesses[i]);
-    }
-    // and then remember the value
-    inputVals[i] = value;
-  }
-}
+//
+//void readSwitches() {
+//  for (byte i = 0; i < count; i += 1) {
+//    // READ switch states
+//    int value = digitalRead(inputPins[i]);
+//
+//    // if on, make sure it was off before
+//    if (value && !inputVals[i]) {
+//      // then add brightness
+//      brightnesses[i] += addBrightness;
+//      // make sure it fits in the range of LED output
+//      brightnesses[i] = constrain(brightnesses[i], initialBrightness, maxBrightness - initialBrightness);
+//      Serial.println(brightnesses[i]);
+//    }
+//    // and then remember the value
+//    inputVals[i] = value;
+//  }
+//}
