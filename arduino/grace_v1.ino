175da131d138b80d40aa1ab1c9d9429a8e1456d9
Shirley Wu <shirleyxueyangwu@gmail.com>
2019-11-26 13:01:44 -0800

    Update grace_v1.ino

--- arduino/grace_v1.ino
+++ arduino/grace_v1.ino
@@ -1,36 +1,38 @@
 #include <Adafruit_NeoPixel.h>
 
 // use #define for read-only integers to save memory
-#define numOrbs 5 // update to number of orbs
-#define numGroups 2 // update to number of groups/categories
+#define numOrbs 5 // UPDATE to number of orbs
+#define numGroups 2 // UPDATE to number of groups/categories
+#define maxOrbsPerGroup 5 // UPDATE
 #define initialBrightness 10
 #define maxBrightness 255
 #define addBrightness 10
 #define debounce 500 // if switch reading is unstable, only keep the reading if it stays constant for >50 secs
+#define duration 3000 // fade animation duration
+#define stagger 200
 // read-only arrays
-const int outputPins[numOrbs] = {3, 5, 6, 9, 10}; // array of pin numbers for LED/neopixels
-const int inputPins[numOrbs] = {2, 11}; // array of pin numbers for tilt switches
-const int orbToGroup[numOrbs][2] = { // for each orb, the group it belongs to and the index of that orb within the group
-  {0, 1}, // so this orb belongs to group 0, and is the first orb in group 0
-  {1, 1},
-  {0, 2}, // and this one is group 0, and second orb in group 0
-  {1, 2}, // I'm keeping track of the index so that we can stagger the fade animation
-  {0, 3}
+const int outputPins[numOrbs] = {3, 5, 6, 9}; // array of pin numbers for LED/neopixels
+const int inputPins[numOrbs] = {11}; // array of pin numbers for tilt switches
+const int groups[numGroups][maxOrbsPerGroup] = {
+  {0, 1, 3}, // group one with index of orbs that belong to it
+  {2}
 };
+int orbToGroup[numOrbs][2]; // this array will be created in setup so we can look up what group an orb belongs to, and what index within that group
 
 // arrays that will be modified as the installation goes on
 unsigned long currentTime = 0;
 Adafruit_NeoPixel neopixels[numOrbs];
-unsigned long switchStates[numOrbs][3]; // keep track of which orb is being interacted with: [0 or 1, millis, debounce 0 or 1]
-int lightBrightnesses[numOrbs]; // keep track of pin brightness
-unsigned long groupStates[numGroups][2]; // keep track of which group is getting lit up: [0 or 1, millis]
+int neutralBrightnesses[numOrbs]; // keep track of pin brightness
+unsigned long switchStates[numOrbs][2]; // [value, prevTime, prevDebounceValue]
+unsigned long lightStates[numOrbs][3]; // [startBrightness, endBrightness, startTime]
+unsigned long groupStates[numGroups][3]; // keep track of which group is getting lit up: [0 or 1, startTime, activeOrb]
 
 void setup() {
   Serial.begin(9600);
   
   for (byte i = 0; i < numOrbs; i += 1) {
     // 1. set default brightness
-    lightBrightnesses[i] = initialBrightness;
+    neutralBrightnesses[i] = initialBrightness;
     // 2. register output pins
     neopixels[i] = Adafruit_NeoPixel(1, outputPins[i]);
     neopixels[i].begin();
@@ -41,18 +43,32 @@ void setup() {
     switchStates[i][1] = currentTime;
     switchStates[i][2] = 0;
   }
+
+  int orb = 0;
+  currentTime = millis();
+  for (byte i = 0; i < numGroups; i += 1) {
+    for (byte j = 0; j < maxOrbsPerGroup; j += 1) {
+      orb = groups[i][j];
+      if (j == 0 || orb) { // HACKY but please don't touch ;___;
+        orbToGroup[orb][0] = i; // remember group number
+        orbToGroup[orb][1] = j; // remember orb index
+      }
+    }
+  }
 }
 
 void loop() {
   currentTime = millis();
-  readSwitches();
-  writeLights();
-  delay(100);
+
+  if (currentTime > 0) {
+    readSwitches();
+    calculateBrightnesses();
+    writeLights();
+  }
 }
 
-void readSwitches() {  
-  // TODO: CHANGE BACK TO numOrbs
-  for (byte i = 0; i < 2; i += 1) {
+void readSwitches() {
+  for (byte i = 0; i < 1; i += 1) { // TODO: CHANGE BACK TO numOrbs
     // READ switch states
     int value = !digitalRead(inputPins[i]);
     int prevValue = switchStates[i][0];
@@ -64,29 +80,25 @@ void readSwitches() {
       switchStates[i][1] = currentTime;
     }
     // DEBOUNCE
-    if ((currentTime - prevTime) > debounce) {
+    if ((currentTime - prevTime) > debounce && value != prevValue) {
       // this is a stable reading, so we can trust it
-      if (value != prevValue) {
-        // if it was previously turned off and is now on:
-        if (value && !prevValue) {
-          // then add brightness
-          lightBrightnesses[i] += addBrightness;
-          // make sure it fits in the range of LED output
-          lightBrightnesses[i] = constrain(lightBrightnesses[i], initialBrightness, maxBrightness - initialBrightness);
-        }
 
-        // if value has changed, update orb state & time 
-        switchStates[i][0] = value;
-        switchStates[i][1] = currentTime;
+      if (!value) {
+        // if it just got turned off, add brightness
+        neutralBrightnesses[i] += addBrightness;
+        // make sure it fits in the range of LED output
+        neutralBrightnesses[i] = constrain(neutralBrightnesses[i], initialBrightness, maxBrightness - initialBrightness); 
       }
-    }
-    
-    // also update its group
-    int group = orbToGroup[i][0];
-    int groupValue = groupStates[group][0];
-    if (value != groupValue) {
+      
+      int group = orbToGroup[i][0];
       groupStates[group][0] = value;
       groupStates[group][1] = currentTime;
+      groupStates[group][2] = orbToGroup[i][1]; // orb index
+      // (if orb is being turned off there's an edge case)
+
+      // if value has changed, update orb state & time 
+      switchStates[i][0] = value;
+      switchStates[i][1] = currentTime;
     }
 
     // set prevDebounceValue to the reading
@@ -94,21 +106,38 @@ void readSwitches() {
   }
 }
 
+void calculateBrightnesses() {
+  int orb = 0;
+  for (byte i = 0; i < numGroups; i += 1) {
+    if (groupStates[i][1] == currentTime) {
+      // if group was just updated
+      for (byte j = 0; j < maxOrbsPerGroup; j += 1) {
+        orb = groups[i][j];
+        if (j == 0 || orb) { // make sure orb exists (HACKY but please don't touch ;___; )
+          if (groupStates[i][0]) { // if group is turned on
+            lightStates[orb][0] = neutralBrightnesses[i]; // start brightness
+            lightStates[orb][1] = maxBrightness; // end brightness
+          } else { // if group is turned off
+            lightStates[orb][0] = maxBrightness;
+            lightStates[orb][1] = neutralBrightnesses[i];
+          }
+
+          int indexDiff = groupStates[i][2]- j;
+          // start time + stagger * distance
+          lightStates[orb][2] = groupStates[i][1] + stagger * abs(indexDiff); // start time
+        }
+      }
+    }
+  }
+}
+
 void writeLights() {
   for (byte i = 0; i < numOrbs; i += 1) {
-    int value = switchStates[i][0];
-    int group = orbToGroup[i][0];
-    int groupValue = groupStates[group][0];
-    
-    if (value) {
-       // if being pressed
-      neopixels[i].setPixelColor(0, maxBrightness, maxBrightness, maxBrightness);
-    } else if (groupValue) {
-      // if group is being lit up
-      neopixels[i].setPixelColor(0, maxBrightness, maxBrightness, maxBrightness);
-    } else {
-      neopixels[i].setPixelColor(0, lightBrightnesses[i], lightBrightnesses[i], lightBrightnesses[i]);
+    // go through each orb and set its brightness
+    if (lightStates[i][2] <= currentTime) { // if current time is after the orb's start time
+      // set orb light to end brightness (for now)
+      neopixels[i].setPixelColor(0, lightStates[i][1], lightStates[i][1], lightStates[i][1]);
+      neopixels[i].show();
     }
-    neopixels[i].show();
   }
 }
