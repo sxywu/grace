ec89da9f26f155e2af501e76e570a2a022168dff
Shirley Wu <shirleyxueyangwu@gmail.com>
2019-12-08 17:46:40 +0800

    start vue app

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+import * as d3 from 'd3'
+
+export default {
+  name: 'app',
+  components: {},
+}
+</script>
+
+<style scoped>
+</style>
