4ec4d7c504a60d318be0b7050e03c2abf9ee4522
Shirley Wu <shirleyxueyangwu@gmail.com>
2020-02-10 23:44:05 -0800

    add ground and sky

--- src/components/World.vue
+++ src/components/World.vue
@@ -13,6 +13,14 @@ import rafLoop from 'raf-loop'
 import * as d3 from 'd3'
 import * as THREE from 'three'
 const OrbitControls = require('three-orbit-controls')(THREE)
+// import { BloomEffect, EffectComposer, EffectPass, RenderPass } from "postprocessing"
+
+const navy = 0x0B1E38
+const purple = 0x362C51
+const blue = 0x51629E
+const yellow = 0xFFE78B
+const pink = 0xDD867E
+const beige = 0xEBBA8B
 
 export default {
   name: 'world',
@@ -37,6 +45,17 @@ export default {
     // WebGL background color
     this.renderer.setClearColor(0xffffff, 0)
 
+    // lights
+    // const hemisphere1 = new THREE.HemisphereLight( purple, blue, 1 )
+    // this.scene.add(hemisphere1)
+    const hemisphere2 = new THREE.HemisphereLight( yellow, navy, 1.0 )
+    this.scene.add(hemisphere2)
+    const ambient = new THREE.AmbientLight(yellow , 1.0 )
+    this.scene.add(ambient)
+    this.light = new THREE.DirectionalLight( yellow, 1.0 )
+    this.light.position.set(0, 100, 100)
+    this.scene.add( this.light )
+
     // set renderer size & pixel ratio
     this.renderer.setSize(this.width, this.height)
     const dpr = Math.min(2, window.devicePixelRatio)
@@ -55,6 +74,7 @@ export default {
     this.clock = new THREE.Clock()
 
     this.createOrbs()
+    this.createBackground()
     this.draw()
   },
   destroyed() {
@@ -82,13 +102,45 @@ export default {
       const starGeometry = new THREE.SphereGeometry(0.1, 20, 20)
       _.each(this.orbs, ({x, y, z}) => {
         const starMaterial = new THREE.MeshBasicMaterial( {
-          color: 0xffffff,
+          color: beige,
           side: THREE.DoubleSide,
         })
         const mesh = new THREE.Mesh(starGeometry, starMaterial)
         mesh.position.set(x, y, z)
         this.scene.add( mesh )
       })
+    },
+    createBackground() {
+      // textured floor inspiration from
+      // https://tympanus.net/codrops/2016/04/26/the-aviator-animating-basic-3d-scene-threejs/
+      const planeSize = 2 * this.maxZPosition
+      const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(planeSize, planeSize, planeSize, planeSize / 2),
+        new THREE.MeshStandardMaterial( {
+          color: purple,
+          side: THREE.DoubleSide,
+      		flatShading: true,
+          roughness: 1.0,
+        } )
+      )
+      _.each(plane.geometry.vertices, v => {
+        v.x += _.random(-0.25, 0.25)
+        v.y += _.random(-0.25, 0.25)
+        v.z += _.random(-0.25, 0.25)
+      })
+      plane.rotateX(-Math.PI / 2)
+      plane.translateZ(-3)
+      this.scene.add( plane )
+
+      // and add "sky"
+      const sky = new THREE.Mesh(
+        new THREE.SphereGeometry(this.maxZPosition / 2 + 10, 20, 20),
+        new THREE.MeshStandardMaterial( {
+          color: purple,
+          side: THREE.BackSide,
+        } )
+      )
+      this.scene.add( sky )
     },
     handleWindowResize() {
       this.renderer.setSize(this.width, this.height)
